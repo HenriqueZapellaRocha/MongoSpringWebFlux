ad7644eaed9f40a12b87565b2ff41bd6e377bb04
henriqueZapellaRocha <henri_zap@hotmail.com>
2024-12-05 15:36:08 -0300

    creating first web flux test

--- src/test/java/com/example/mongospringwebflux/controllers/ProductControllerTest.java
+++ src/test/java/com/example/mongospringwebflux/controllers/ProductControllerTest.java
@@ -1,20 +1,50 @@
 package com.example.mongospringwebflux.controllers;
 
+import com.example.mongospringwebflux.MockBuiilders.MockBuilder;
 import com.example.mongospringwebflux.service.services.ProductService;
-import com.example.mongospringwebflux.v1.controller.ProductController;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import com.example.mongospringwebflux.v1.controller.ProductResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
-@WebFluxTest(controllers = ProductController.class)
-@ExtendWith({SpringExtension.class, MockitoExtension.class})
+@AutoConfigureWebTestClient
 class ProductControllerTest {
 
-    @Mock
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @MockBean
     private ProductService productService;
+
+
+    @Test
+    void productControllerTestAddProductReturnOk() {
+        // Mock da resposta do serviço
+        var  productResponse = MockBuilder.productResponse();
+        when(productService.getById(anyString(),anyString(),anyString())).thenReturn(Mono.just(productResponse));
+
+        // Execução do teste
+        WebTestClient.ResponseSpec response = webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/product/"+productResponse.productID())
+                        .queryParam("currency", "BRL")
+                        .build())
+                .exchange();
+
+        // Validação do status da resposta
+        response.expectStatus().isOk();
+    }
+
 
 }
