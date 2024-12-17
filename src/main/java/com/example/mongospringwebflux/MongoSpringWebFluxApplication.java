1c904e24971fd702e9a4aa7fb31bb58bfd457c0e
henriqueZapellaRocha <henri_zap@hotmail.com>
2024-12-17 12:18:44 -0300

    swagger now works

--- src/main/java/com/example/mongospringwebflux/MongoSpringWebFluxApplication.java
+++ src/main/java/com/example/mongospringwebflux/MongoSpringWebFluxApplication.java
@@ -1,12 +1,15 @@
 package com.example.mongospringwebflux;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Documentation APIs v1.0"))
 public class MongoSpringWebFluxApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/mongospringwebflux/configs/security/SecurityConfig.java
+++ src/main/java/com/example/mongospringwebflux/configs/security/SecurityConfig.java
@@ -46,10 +46,10 @@ public class SecurityConfig {
                         .pathMatchers("/admin/**")
                                             .hasAuthority("ROLE_ADMIN")
 
-                        .pathMatchers( "/v3/api-docks/**", "swagger-ui/**", "swagger-ui.html" )
+                        .pathMatchers( "/v3/api-docks/**", "swagger-ui/**", "swagger-ui.html", "/swagger-ui.html"  )
                                                 .permitAll()
 
-                        .anyExchange().authenticated()
+                        .anyExchange().permitAll()
                 )
                 .addFilterBefore( securityFilter, SecurityWebFiltersOrder.AUTHORIZATION )
                 .build();
