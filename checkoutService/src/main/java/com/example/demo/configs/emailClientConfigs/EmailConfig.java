ffba6b4d4527ada331445c36bcac72c0141f2a97
henriqueZapellaRocha <henri_zap@hotmail.com>
2025-02-10 15:10:01 -0300

    refining email config

--- checkoutService/src/main/java/com/example/demo/configs/emailClientConfigs/EmailConfig.java
+++ checkoutService/src/main/java/com/example/demo/configs/emailClientConfigs/EmailConfig.java
@@ -15,29 +15,23 @@ public class EmailConfig {
 
     @Value("${mail.host}")
     private String hostname;
-
     @Value("${mail.port}")
     private int port;
-
     @Value("${mail.username}")
     private String username;
-
     @Value("${mail.password}")
     private String password;
-
     @Value("${mail.ssl}")
     private boolean starttls;
 
     @Bean
     public MailClient mailClient() {
-        // Configuração do MailClient a partir das propriedades
         MailConfig config = new MailConfig()
                 .setHostname(hostname)
                 .setPort(port)
                 .setUsername(username)
                 .setPassword(password)
                 .setStarttls(StartTLSOptions.REQUIRED);
-//                .setSsl(true);
         return MailClient.createShared(Vertx.vertx(), config);
     }
 }
